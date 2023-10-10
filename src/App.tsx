3aa21c1259aedac88d7b5903987614b291ef2cbb
Evan Phillips <ephill18@gmail.com>
2023-10-09 23:02:42 -0700

    add clipboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import { fetchWord } from "./fetchWord";
 
@@ -12,6 +12,12 @@ function App() {
       }
     );
   }, []);
+
+  const handleCopyClick = useCallback(() => {
+    if (words) {
+      navigator.clipboard.writeText(words.join(" "));
+    }
+  }, [words]);
 
   return (
     <>
@@ -30,9 +36,26 @@ function App() {
           .
         </h3>
         {words && (
-          <p className="font-mono text-xl mb-4 border-2 border-black w-max px-4 py-2 rounded-md bg-white dark:bg-black max-w-full">
-            {words.join(" ")}
-          </p>
+          <div className="font-mono text-xl mb-4 border-2 border-black py-1 rounded-md bg-white dark:bg-black max-w-full flex flex-row items-center justify-center">
+            <p className="grow border-r border-neutral-700 dark:border-neutral-200 px-3">
+              {words.join(" ")}
+            </p>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 m-3 hover:stroke-neutral-400 hover:cursor-pointer"
+              onClick={handleCopyClick}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
+              />
+            </svg>
+          </div>
         )}
       </main>
       <footer className="relative shadow-lg bg-neutral-50 flex items-center justify-center dark:bg-neutral-700 dark:text-neutral-200">
